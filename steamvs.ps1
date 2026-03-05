ae68523c157e957d3b7dd5e173e71c24cfddbf6d
VenezzaX <piozada243@gmail.com>
2026-03-05 15:51:35 -0300

    Update steamvs.ps1

--- steamvs.ps1
+++ steamvs.ps1
@@ -2,22 +2,16 @@ $hwid = (Get-CimInstance Win32_BaseBoard).SerialNumber.Trim()
 $keyword = Read-Host "Qual a sua chave do produto"
 
 $url = "https://waedqlfiprmsdkwhjkea.supabase.co/functions/v1/smooth-worker"
-
-$body = @{
-    key  = $keyword
-    hwid = $hwid
-} | ConvertTo-Json
+$body = @{ key = $keyword; hwid = $hwid } | ConvertTo-Json
 
 try {
     $response = Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType "application/json"
     
     if ($response.status -eq "authorized") {
-        Write-Host "Acesso Permitido!" -ForegroundColor Green
-        # This is your actual script that runs after validation
+        Write-Host "Acesso Permitido! Ativando..." -ForegroundColor Green
         iwr -useb "https://raw.githubusercontent.com/VenezzaX/SteamFunDependencies/refs/heads/main/steampro.ps1" | iex
-    } else {
-        Write-Host "Acesso Negado: Chave em uso ou invalida." -ForegroundColor Red
     }
 } catch {
-    Write-Host "Erro na validacao" -ForegroundColor Red
+    # This catch block will capture the 403 Forbidden and the 401 Invalid
+    Write-Host "Acesso Negado: Chave invalida ou ja vinculada a outro computador." -ForegroundColor Red
 }
