4cf080bbf82f2a46e40dda7ea4a9e346b8edfd49
VenezzaX <piozada243@gmail.com>
2026-03-06 01:35:23 -0300

    Update steamvs.ps1

--- steamvs.ps1
+++ steamvs.ps1
@@ -1,17 +1,32 @@
-$hwid = (Get-CimInstance Win32_BaseBoard).SerialNumber.Trim()
-$keyword = Read-Host "Qual a sua chave do produto"
 
-$url = "https://waedqlfiprmsdkwhjkea.supabase.co/functions/v1/smooth-worker"
-$body = @{ key = $keyword; hwid = $hwid } | ConvertTo-Json
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Add-Type -AssemblyName Microsoft.VisualBasic, System.Windows.Forms
+
+
+$hwid = (Get-CimInstance Win32_BaseBoard).SerialNumber.Trim()
+$key = [Microsoft.VisualBasic.Interaction]::InputBox("Insira sua chave de ativação:", "Ativação do Sistema", "")
+
+if (-not $key) { exit }
 
 try {
-    $response = Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType "application/json"
-    
-    if ($response.status -eq "authorized") {
-        Write-Host "Acesso Permitido! Ativando..." -ForegroundColor Green
-        iwr -useb "https://raw.githubusercontent.com/VenezzaX/SteamFunDependencies/refs/heads/main/steampro.ps1" | iex
+
+    $url = "https://waedqlfiprmsdkwhjkea.supabase.co/functions/v1/smooth-worker"
+    $body = @{ key = $key; hwid = $hwid } | ConvertTo-Json
+    $auth = Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType "application/json" -UserAgent "Mozilla/5.0"
+
+    if ($auth.status -eq "authorized") {
+        
+        Write-Host "----------------------------------------------------" -ForegroundColor Cyan
+        Write-Host "         SERVICO ATIVADO PERMANENTEMENTE            " -ForegroundColor Green
+        Write-Host "----------------------------------------------------" -ForegroundColor Cyan
+        Write-Host "(Qualquer erro ative o novamente com a mesma chave)" -ForegroundColor Yellow
+        Write-Host ""
+
+        IEX (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/VenezzaX/SteamFunDependencies/refs/heads/main/steampro.ps1" -UseBasicParsing)
+
+    } else {
+        [System.Windows.Forms.MessageBox]::Show("Chave inválida ou já vinculada a outro computador.", "Acesso Negado")
     }
 } catch {
-    # This catch block will capture the 403 Forbidden and the 401 Invalid
-    Write-Host "Acesso Negado: Chave invalida ou ja vinculada a outro computador." -ForegroundColor Red
+    [System.Windows.Forms.MessageBox]::Show("Erro de conexão com o servidor. Verifique sua internet.", "Erro")
 }
