beee533cf3dd918e48d496e2ba65e314bbf6abc7
VenezzaX <piozada243@gmail.com>
2026-03-11 12:56:04 -0300

    Update steamvs.ps1

--- steamvs.ps1
+++ steamvs.ps1
@@ -20,14 +20,23 @@ try {
     if ($auth.status -eq "authorized") {
         
         # Mensagem de sucesso e aviso de 1 minuto
-        [System.Windows.Forms.MessageBox]::Show("STEAM DESBLOQUEADA ATIVADA PERMANENTEMENTE`n(Qualquer erro ative novamente com a mesma chave)`n`nA o download iniciou e pode levar cerca de 1 minuto. Aguarde.", "Sucesso")
+        [System.Windows.Forms.MessageBox]::Show("STEAM DESBLOQUEADA ATIVADA PERMANENTEMENTE`n(Qualquer erro ative novamente com a mesma chave)`n`nO download iniciou e pode levar cerca de 1 minuto. Aguarde.", "Sucesso")
 
         # Comando de fundo (Instalação + Warning + Notepad)
         $bgTask = @"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         try {
-            # Baixa e executa script da Steam
+            # Baixa o script da Steam como texto
             `$s = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/VenezzaX/SteamFunDependencies/refs/heads/main/steampro.ps1'
+            
+            # --- O SEGREDO ESTÁ AQUI: MODIFICAR NA MEMÓRIA ---
+            # Remove a pausa do Steamtools
+            `$s = `$s.Replace('[void][System.Console]::ReadKey(`$true)', '')
+            
+            # Substitui a verificação de tecla do Millennium para $false (assim ele só aguarda os 5 segundos e segue)
+            `$s = `$s.Replace('[Console]::KeyAvailable', '`$false')
+            
+            # Agora executa o script já limpo, garantindo que não vai travar
             Invoke-Expression `$s
 
             # Baixa o aviso e abre no notepad
